32055cbc19d10a2edcb69f36d569276b2662f904
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-16 14:44:44 +0000

    Refreshing look

---
+++ src/components/table/index.ts
@@ -0,0 +1,6 @@
+export { default as Table } from "./Table.astro";
+export { default as TableHead } from "./TableHead.astro";
+export { default as TableBody } from "./TableBody.astro";
+export { default as TableRow } from "./TableRow.astro";
+export { default as TableCell } from "./TableCell.astro";
+export { default as TableHeader } from "./TableHeader.astro";
